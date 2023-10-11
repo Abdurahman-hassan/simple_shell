1a9448044e3ad93ca4184db393d3829205c6b72c
mojtaba <mojtaba.mohamed187@gmail.com>
2023-10-11 21:37:40 +0300

    change comment style

--- shell.c
+++ shell.c
@@ -79,58 +79,60 @@ void run_interactive(char *av)
 
 void run_noninteractive(char *av)
 {
-	char *buf = NULL, **path;
-	pid_t child_pid;
-	int get, status;
-	size_t n = 0; // Let getline allocate with malloc/realloc
+    char *buf = NULL, **path;
+    pid_t child_pid;
+    int get, status;
+    size_t n = 0; /* Let getline allocate with malloc/realloc */
 
-	while ((get = getline(&buf, &n, stdin)) != -1) {  // Keep reading lines until EOF
-		/*Remove the trailing newline character*/
-		buf[strcspn(buf, "\n")] = '\0';
+    while ((get = getline(&buf, &n, stdin)) != -1) 
+    {   
+         /* Keep reading lines until EOF */
+         /*Remove the trailing newline character*/
+        buf[strcspn(buf, "\n")] = '\0';
 
-		/*Exit the shell when the user types 'exit'*/
-		if (strcmp(buf, "exit") == 0) {
-			free(buf);
-			exit(EXIT_SUCCESS);  // It's better to exit successfully when user types 'exit'
-		}
+        /*Exit the shell when the user types 'exit'*/
+        if (strcmp(buf, "exit") == 0) {
+            free(buf);
+            exit(EXIT_SUCCESS);  /* It's better to exit successfully when user types 'exit'*/
+        }
 
-		if (strcmp(buf, "env") == 0) {
-			_env();
-			continue; // Skip the rest of the loop for 'env'
-		}
+        if (strcmp(buf, "env") == 0) {
+            _env();
+            continue; /* Skip the rest of the loop for 'env' */
+        }
 
-		path = split_string(buf, " ");
+        path = split_string(buf, " ");
 
-		if (access(path[0], F_OK | X_OK) == -1)
-		{
-			printf("%s: %s\n", av, strerror(errno));
-			free(buf); // free the buffer before continuing
-			free(path); // free the path before continuing
-			continue;
-		}
+        if (access(path[0], F_OK | X_OK) == -1)
+        {
+            printf("%s: %s\n", av, strerror(errno));
+            free(buf); /* free the buffer before continuing */
+            free(path); /* free the path before continuing */
+            continue;
+        }
 
-		child_pid = fork(); /*Create a child process*/
-		if (child_pid == -1) {
-			perror(av); // Use perror for system error messages
-			free(buf);
-			free(path); // free the path on error
-			exit(EXIT_FAILURE);
-		}
+        child_pid = fork(); /* Create a child process */
+        if (child_pid == -1) {
+            perror(av); /* Use perror for system error messages */
+            free(buf);
+            free(path); /* free the path on error */
+            exit(EXIT_FAILURE);
+        }
 
-		if (child_pid == 0) { /*Inside the child process*/
-			/*Execute the command*/
-			if (execve(path[0], path, NULL) == -1)
-			{
-				perror(av);
-				free(buf);
-				free(path); // free the path on error
-				exit(EXIT_FAILURE); // Child process should exit on failure
-			}
-		} else {
-			wait(&status);  /*Make the parent process wait until the child completes*/
-		}
-		free(path); // free the path after use
-	}
-	free(buf);  // Free the buffer at the end
+        if (child_pid == 0) { 
+            /*Inside the child process*/
+            /*Execute the command*/
+            if (execve(path[0], path, NULL) == -1)
+            {
+                perror(av);
+                free(buf);
+                free(path); /* free the path on error */
+                exit(EXIT_FAILURE); /* Child process should exit on failure */
+            }
+        } else {
+            wait(&status);  /*Make the parent process wait until the child completes*/
+        }
+        free(path); /* free the path after use */
+    }
+    free(buf);  /* Free the buffer at the end */
 }
-
