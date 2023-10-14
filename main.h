ad96eff64119e8abca2cc4ce0b4d4aedb0ecdb0d
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-10-14 05:33:08 +0000

    re-format main.h

--- main.h
+++ main.h
@@ -20,30 +20,43 @@
 
 extern char **environ;
 
+
+/* ============ INTERACTIVE FUNCTION ============== */
 void run_interactive(char *av);
+
+/* ============ NON_INTERACTIVE FUNCTION ============== */
+
 void run_noninteractive(char *av);
+
+/* ===========  UTILS ============ */
+
 unsigned int _strlen(char *s);
-void _env(void);
-
-char **split_string(char *string, char *delim);
-int count_tokens(char *string, char *delim);
-
-void free_path(char **path);
-int my_atexit(void (*function)(void));
-void _exit(int status);
-int *get_status(void);
-int isempty(char *str);
-int _isalpha(int c);
-
-ssize_t _read_chars(char **lineptr, size_t *n, int fd);
-ssize_t _getline(char **lineptr, size_t *n, int fd);
-
-char *search_dir(char *filename);
-void not_found_err(char *av, char *cmd);
-char *_strdup(char *str);
 char *_strcpy(char *dest, char *src);
 char *_strcat(char *dest, char *src);
 char *_strchr(char *s, char c);
 int _strcmp(char *s1, char *s2);
+
+/* =============  UTILS_2 ============ */
+
+char *_strdup(char *str);
+char *search_dir(char *filename);
+int count_tokens(char *string, char *delim);
+char **split_string(char *string, char *delim);
+
+/* =============  UTILS_3 =============== */
+
+void free_path(char **path);
+int my_atexit(void (*function)(void));
+void _exit(int status);
+ssize_t _read_chars(char **lineptr, size_t *n, int fd);
+ssize_t _getline(char **lineptr, size_t *n, int fd);
+
+/* ============== UTILS_4 ================ */
+
+int *get_status(void);
+int isempty(char *str);
+int _isalpha(int c);
+void not_found_err(char *av, char *cmd);
+void _env(void);
 
 #endif /* SIMPLE_SHELL_MAIN_H */
