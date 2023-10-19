60bbc036adee47688e3b96cba39b7f5192c0b3d4
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2023-10-19 05:06:44 +0300

    Add description to the custom_log function, fix linting

--- utils_13.c
+++ utils_13.c
@@ -1,5 +1,20 @@
 #include "main.h"
 
+/**
+ * custom_log - Logs formatted strings to stderr.
+ * @format: Format string following printf standards.
+ * This function logs messages to the
+ * standard error stream, accepting
+ * printf-like format specifiers.
+ * It handles variable argument lists,
+ * formats the message, prints it to stderr,
+ * and then frees the allocated
+ * memory. The caller does not need
+ * to manage memory, as this is handled
+ * internally. Note that 'vasprintf' is used
+ * for formatting, which is a GNU
+ * extension and may affect portability.
+ */
 void custom_log(const char *format, ...)
 {
 	va_list args;
